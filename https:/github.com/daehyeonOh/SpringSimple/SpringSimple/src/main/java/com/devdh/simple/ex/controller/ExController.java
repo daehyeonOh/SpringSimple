9d94dbab1032c87f19e49eb8cb7736c7cbad59c4
dh <aquarius-dh@naver.com>
2019-02-15 03:02:10 +0900

    .

--- https:/github.com/daehyeonOh/SpringSimple/SpringSimple/src/main/java/com/devdh/simple/ex/controller/ExController.java
+++ https:/github.com/daehyeonOh/SpringSimple/SpringSimple/src/main/java/com/devdh/simple/ex/controller/ExController.java
@@ -2,6 +2,8 @@ package com.devdh.simple.ex.controller;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devdh.simple.service.ExService;
@@ -9,6 +11,8 @@ import com.devdh.simple.service.ExService;
 @RestController
 public class ExController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ExController.class);
+	
 	@Inject
 	ExService exService;
 	
